4c60d4890b0b1674cdb356eb737d373f558e1aed
Ilya Palachev (home) <iliyapalachev@gmail.com>
2013-03-08 14:23:58 +0400

    [Refactor] Now globalCorrectionCube takes input args

--- tests/globalCorrectionCube.cpp
+++ tests/globalCorrectionCube.cpp
@@ -6,10 +6,34 @@
 #include "Polyhedron.h"
 #include "Gauss_test.h"
 
+void printUsage()
+{
+	printf("Usage: \n"
+			"./globalCorrectionCube <number_of_contours> <max_move_delta>\n");
+}
+
 int main(int argc, char** argv) {
+
+	int numContours;
+	double maxMoveDelta;
+
+	if (argc != 3)
+	{
+		printUsage();
+		return 1;
+	}
+	if( sscanf(argv[1], "%d", &numContours) != 1)
+	{
+		printUsage();
+		return 2;
+	}
+	if ( sscanf(argv[1], "%lf", &maxMoveDelta) != 1)
+	{
+		printUsage();
+		return 3;
+	}
     Polyhedron poly;
-    printf("Polyhedron has been created...\n");
-    poly.corpolTest(4, 0);
+    poly.corpolTest(numContours, maxMoveDelta);
     return 0;
 }
 
