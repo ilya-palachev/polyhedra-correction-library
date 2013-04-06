263a0a27ff4c1bac050861bd047e315b9151344c
Ilya Palachev (home) <iliyapalachev@gmail.com>
2013-04-06 12:38:43 +0400

    [IMPL] Add macro for error debug print output

--- Include/DebugPrint.h
+++ Include/DebugPrint.h
@@ -9,21 +9,32 @@
 //#define TCPRINT //Печатать вывод из Polyhedron::test_consections()
 #define GLOBAL_CORRECTION_DERIVATIVE_TESTING
 
+#define COLOUR_NORM  		"\x1B[0m"
+#define COLOUR_RED  		"\x1B[31m"
+#define COLOUR_GREEN  	"\x1B[32m"
+#define COLOUR_YELLOW  	"\x1B[33m"
+#define COLOUR_BLUE 		"\x1B[34m"
+#define COLOUR_MAGENTA  "\x1B[35m"
+#define COLOUR_CYAN  		"\x1B[36m"
+#define COLOUR_WHITE  	"\x1B[37m"
+
 #define WHERESTR  "[%s: %d]: "
 #define WHEREARG  __func__, __LINE__
-#define DBGPRINT2(...)       fprintf(stderr, __VA_ARGS__)
+#define STDERR_PRINT(...)       fprintf(stderr, __VA_ARGS__)
 
 #ifndef NDEBUG
-#define DBGPRINT(_fmt, ...)  DBGPRINT2(WHERESTR _fmt "\n", WHEREARG, ##__VA_ARGS__)
+#		define DBGPRINT(_fmt, ...)  STDERR_PRINT(COLOUR_NORM WHERESTR _fmt "\n", WHEREARG, ##__VA_ARGS__)
+#		define ERROR_PRINT(_fmt, ...)  STDERR_PRINT(COLOUR_RED WHERESTR _fmt "\n", WHEREARG, ##__VA_ARGS__)
+#		define DBG_START DBGPRINT("Start")
+#		define DBG_END DBGPRINT("End")
 #else
-#define DBGPRINT(_fmt, ...)
+#		define DBGPRINT(_fmt, ...)
+#		define ERROR_PRINT(_fmt, ...)
+#		define DBG_START
+#		define DBG_END
 #endif
 
-#define DBG_START \
-	DBGPRINT("Start\n");
 
-#define DBG_END \
-	DBGPRINT("End\n");
 
 #endif //DEBUGPRINT_H
 
