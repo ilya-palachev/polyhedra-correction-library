32ddead83ae911be82c3c3b4eefc9ff5e93d3ebc
Ilya Palachev (home) <iliyapalachev@gmail.com>
2013-04-27 14:20:33 +0400

    [DEBUG] Add debug output and asertion of test_initialization() to area calc

--- Source/Facet/Facet_area.cpp
+++ Source/Facet/Facet_area.cpp
@@ -11,14 +11,23 @@ double Facet::area() {
 	DEBUG_START;
 	my_fprint_all(stderr);
 
+	ASSERT(test_initialization());
 	Vector3d A0 = parentPolyhedron->vertices[indVertices[0]];
+	my_fprint_all(stderr);
+	DEBUG_PRINT("  in parent polyhedron there are %d vertices",
+			parentPolyhedron->numVertices);
 
 	double areaFacet = 0.;
 	for (int iVertex = 1; iVertex < numVertices - 1; ++iVertex) {
+		DEBUG_PRINT("  calculating area of triangle %d : 1 begin", iVertex);
 		Vector3d A1 = parentPolyhedron->vertices[indVertices[iVertex]] - A0;
+		DEBUG_PRINT("  calculating area of triangle %d : 2", iVertex);
 		Vector3d A2 = parentPolyhedron->vertices[indVertices[iVertex + 1]] - A0;
+		DEBUG_PRINT("  calculating area of triangle %d : 3", iVertex);
 		double areaTriangle = (A1 % A2) * plane.norm * 0.5;
+		DEBUG_PRINT("  calculating area of triangle %d : 4", iVertex);
 		areaFacet += areaTriangle;
+		DEBUG_PRINT("  calculating area of triangle %d : 5 end", iVertex);
 	}
 
 	DEBUG_END;
