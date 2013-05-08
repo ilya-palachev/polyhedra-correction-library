5cd8557bd9cd42ff1c5643972306d3bdb72351db
Ilya Palachev (home) <iliyapalachev@gmail.com>
2013-05-08 15:15:24 +0400

    [DEBUG] Change debug output in Facet::area

--- Source/Facet/Facet_area.cpp
+++ Source/Facet/Facet_area.cpp
@@ -21,17 +21,14 @@ double Facet::area()
 	double areaFacet = 0.;
 	for (int iVertex = 1; iVertex < numVertices - 1; ++iVertex)
 	{
-		DEBUG_PRINT("  calculating area of triangle %d : 1 begin", iVertex);
 		Vector3d A1 = parentPolyhedron->vertices[indVertices[iVertex]] - A0;
-		DEBUG_PRINT("  calculating area of triangle %d : 2", iVertex);
 		Vector3d A2 = parentPolyhedron->vertices[indVertices[iVertex + 1]] - A0;
-		DEBUG_PRINT("  calculating area of triangle %d : 3", iVertex);
 		double areaTriangle = (A1 % A2) * plane.norm * 0.5;
-		DEBUG_PRINT("  calculating area of triangle %d : 4", iVertex);
+		DEBUG_PRINT("\tarea of triangle # %d = %lf", iVertex - 1, areaTriangle);
 		areaFacet += areaTriangle;
-		DEBUG_PRINT("  calculating area of triangle %d : 5 end", iVertex);
 	}
 
+	DEBUG_PRINT("area of facet = %lf", areaFacet);
 	DEBUG_END;
 	return areaFacet;
 }
