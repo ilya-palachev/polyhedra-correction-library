5cf1d099c49987a52ee4c6aeb74a4a0def5bfc09
Ilya Palachev (home) <iliyapalachev@gmail.com>
2013-05-08 12:42:26 +0400

    [DEBUG] Add some debug output to Edge::my_fprint

--- Source/Edge/Edge_io.cpp
+++ Source/Edge/Edge_io.cpp
@@ -3,7 +3,8 @@
 void Edge::my_fprint(FILE* file)
 {
 #ifndef NDEBUG
-	fprintf(file, "Printing content of edge #%d", id);
+	fprintf(file, COLOUR_YELLOW "Printing content of edge #%d" COLOUR_WHITE,
+			id);
 	fprintf(file, "\tid = %d (id of the edge)\n", id);
 	fprintf(file, "\tv0 = %d (first vertex)\n", v0);
 	fprintf(file, "\tv1 = %d (second vertex)\n", v1);
@@ -18,13 +19,19 @@ void Edge::my_fprint(FILE* file)
 			"\ti\t|\tid of contour\t|\tnearest side\t|\tdirection\t|\tweight\n");
 
 	int i = 0;
+	int numContoursEssential = 0;
 	for (list<EdgeContourAssociation>::iterator iter = assocList.begin();
 			iter != assocList.end(); ++iter, ++i)
 	{
-
 		fprintf(file, "\t%d\t|\t%d\t\t|\t%d\t\t|\t%d\t\t|\t%lf\n", i,
 				iter->indContour, iter->indNearestSide, iter->ifProperDirection,
 				iter->weight);
+		if (iter->weight >= THRESHOLD_ESSENTIAL_ASSOCIATION)
+		{
+			++numContoursEssential;
+		}
 	}
+	fprintf(file, "\t number of essential contours: %d (weight >= 0.1)\n",
+			numContoursEssential);
 #endif
 }
