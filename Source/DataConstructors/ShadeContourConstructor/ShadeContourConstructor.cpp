993f16d29075b71aed1301afd05339b4e6a25dec
Ilya Palachev (home) <iliyapalachev@gmail.com>
2013-05-17 10:32:44 +0400

    [REFACTOR] Adding fields for data buffers in ShadeContourConstructor

--- Source/DataConstructors/ShadeContourConstructor/ShadeContourConstructor.cpp
+++ Source/DataConstructors/ShadeContourConstructor/ShadeContourConstructor.cpp
@@ -5,22 +5,54 @@
  *      Author: iliya
  */
 
-#include "ShadeContourConstructor.h"
+#include "PolyhedraCorrectionLibrary.h"
 
 ShadeContourConstructor::ShadeContourConstructor(const Polyhedron* p,
 		const ShadeContourData* d) :
 				polyhedron(p),
-				data(d)
+				data(d),
+				bufferBool(NULL),
+				bufferInt0(NULL),
+				bufferInt1(NULL)
 {
 }
 
 ShadeContourConstructor::~ShadeContourConstructor()
 {
+	if (bufferBool != NULL)
+	{
+		delete[] bufferBool;
+		bufferBool = NULL;
+	}
+	if (bufferInt0 != NULL)
+	{
+		delete[] bufferInt0;
+		bufferInt0 = NULL;
+	}
+	if (bufferInt1 != NULL)
+	{
+		delete[] bufferInt1;
+		bufferInt1 = NULL;
+	}
 }
 
-void ShadeContourConstructor::buildShadeContours(int numContoursNeeded,
-		double firstAngle)
+void ShadeContourConstructor::run(int numContoursNeeded, double firstAngle)
 {
+	DEBUG_START;
+	DEBUG_PRINT("Allocating 3 arrays of length %d", numEdges);
+	bool* bufferBool = new bool[numEdges];
+	int* bufferInt0 = new int[numEdges];
+	int* bufferInt1 = new int[numEdges];
 
+	for (int icont = 0; icont < data->numContours; ++icont)
+	{
+		double angle = firstAngle + 2 * M_PI * (icont) / data->numContours;
+		Vector3d nu = Vector3d(cos(angle), sin(angle), 0);
+		Plane planeOfProjection = Plane(nu, 0);
+
+		data->contours[icont] = createContour(icont, planeOfProjection,
+				bufferBool, bufferInt0, bufferInt1);
+	}
+	DEBUG_END;
 }
 
