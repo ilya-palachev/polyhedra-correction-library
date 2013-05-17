d5f68d4cee4ed0aa9b06feb77f1a0f1185dee626
Ilya Palachev (home) <iliyapalachev@gmail.com>
2013-05-17 09:15:55 +0400

    [REFACTOR] Move class GSAssociator to separate header

---
+++ Include/Correctors/GSAssociator.h
@@ -0,0 +1,57 @@
+/*
+ * GSAssociator.h
+ *
+ *  Created on: 17.05.2013
+ *      Author: iliya
+ */
+
+#ifndef GSASSOCIATOR_H_
+#define GSASSOCIATOR_H_
+
+class GSAssociator: public GlobalShadeCorrector
+{
+private:
+	int iContour;
+	int iFacet;
+	int iEdge;
+
+	Polyhedron* polyhedronTmp;
+	double* bufDouble;
+
+	Vector3d v0_projected;
+	Vector3d v1_projected;
+
+	Vector3d v0_nearest;
+	Vector3d v1_nearest;
+
+	int iSideDistMin0;
+	int iSideDistMin1;
+
+	void preinit();
+
+	void run(int iContourIn, int iFacetIn, int iEdgeIn);
+
+	int init();
+	int checkVisibility();
+	double calculateVisibility();
+	int checkAlreadyAdded();
+	int projectEdge();
+	int checkExtinction();
+
+	int findNearestPoint(Vector3d v_projected, Vector3d& v_nearest);
+
+	double calculateArea(Orientation orientation);
+
+	void add(Orientation orientation);
+	void findBounds(Orientation orientation, int& iResultBegin,
+			int& iResultEnd);
+
+	double calculateWeight();
+
+public:
+	GSAssociator();
+	~GSAssociator();
+};
+
+
+#endif /* GSASSOCIATOR_H_ */
--- Include/Correctors/GlobalShadeCorrector.h
+++ Include/Correctors/GlobalShadeCorrector.h
@@ -155,49 +155,4 @@ public:
 	void runCorrection();
 };
 
-class GSAssociator: public GlobalShadeCorrector
-{
-private:
-	int iContour;
-	int iFacet;
-	int iEdge;
-
-	Polyhedron* polyhedronTmp;
-	double* bufDouble;
-
-	Vector3d v0_projected;
-	Vector3d v1_projected;
-
-	Vector3d v0_nearest;
-	Vector3d v1_nearest;
-
-	int iSideDistMin0;
-	int iSideDistMin1;
-
-	void preinit();
-
-	void run(int iContourIn, int iFacetIn, int iEdgeIn);
-
-	int init();
-	int checkVisibility();
-	double calculateVisibility();
-	int checkAlreadyAdded();
-	int projectEdge();
-	int checkExtinction();
-
-	int findNearestPoint(Vector3d v_projected, Vector3d& v_nearest);
-
-	double calculateArea(Orientation orientation);
-
-	void add(Orientation orientation);
-	void findBounds(Orientation orientation, int& iResultBegin,
-			int& iResultEnd);
-
-	double calculateWeight();
-
-public:
-	GSAssociator();
-	~GSAssociator();
-};
-
 #endif /* GLOBALSHADECORRECTOR_H_ */
