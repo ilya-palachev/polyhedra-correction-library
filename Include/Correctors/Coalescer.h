6dcec65138bc27971d22d87a247d066091f0a216
Ilya Palachev (Zhirnovsk) <iliyapalachev@gmail.com>
2013-06-19 13:39:32 +0400

    [REFACTOR] Create class Coalescer

---
+++ Include/Correctors/Coalescer.h
@@ -0,0 +1,20 @@
+/*
+ * Coalescer.h
+ *
+ *  Created on: 19.06.2013
+ *      Author: ilya
+ */
+
+#ifndef COALESCER_H_
+#define COALESCER_H_
+
+#include "PCorrector.h"
+
+class Coalescer: public PCorrector
+{
+public:
+	Coalescer();
+	~Coalescer();
+};
+
+#endif /* COALESCER_H_ */
---
+++ Source/Correctors/Coalescer/Coalescer.cpp
@@ -0,0 +1,17 @@
+/*
+ * Coalescer.cpp
+ *
+ *  Created on: 19.06.2013
+ *      Author: ilya
+ */
+
+#include "Coalescer.h"
+
+Coalescer::Coalescer()
+{
+}
+
+Coalescer::~Coalescer()
+{
+}
+
