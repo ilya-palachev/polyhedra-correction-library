c4acf67586a8bd4863bda02e833aa56bd976ce98
Ilya Palachev (home) <iliyapalachev@gmail.com>
2013-07-02 09:14:54 +0400

    [IMPL] Add function fscanDefault to class ShadeContourData

--- Source/DataContainers/ShadeContourData/ShadeContourData.cpp
+++ Source/DataContainers/ShadeContourData/ShadeContourData.cpp
@@ -23,3 +23,125 @@ ShadeContourData::~ShadeContourData()
 		contours = NULL;
 	}
 }
+
+#define STD_SC_FORMAT_HEADER_SIZE_1 1
+#define STD_SC_FORMAT_HEADER_SIZE_2 4
+
+void ShadeContourData::fscanDefault(char* fileNameContours)
+{
+	FILE* fd = (FILE*) fopen(fileNameContours, "r");
+
+	if (!fd)
+	{
+		ERROR_PRINT("Failed to open file %s", fileNameContours);
+		return;
+	}
+
+	char* scannedString = new char[255];
+	for (int i = 0; i < STD_SC_FORMAT_HEADER_SIZE_1; ++i)
+	{
+		if (fscanf(fd, "%s", scannedString) != 1)
+		{
+			ERROR_PRINT("Wrong file format, in header #1");
+			return;
+		}
+	}
+
+	if (fscanf(fd, "%d", &numContours) != 1)
+	{
+		ERROR_PRINT("Wrong file format, in number of contours");
+		return;
+	}
+
+	for (int i = 0; i < STD_SC_FORMAT_HEADER_SIZE_2; ++i)
+	{
+		if (fscanf(fd, "%s", scannedString) != 1)
+		{
+			ERROR_PRINT("Wrong file format, in header #2");
+			return;
+		}
+	}
+
+	contours = new SContour[numContours];
+	for (int iContour = 0; iContour < numContours; ++iContour)
+	{
+		if (fscanf(fd, "%s", scannedString) != 0)
+		{
+			ERROR_PRINT("Wrong file format, in empty line before contour #%d",
+					iContour);
+			return;
+		}
+
+		SContour* currContour = &contours[iContour];
+
+		if (fscanf(fd, "%d", &currContour->ns) != 1)
+		{
+			ERROR_PRINT("Wrong file format, number of sides for contour #%d",
+					iContour);
+			return;
+		}
+
+		if (fscanf(fd, "%lf", &currContour->plane.norm.x) != 1
+				&& fscanf(fd, "%lf", &currContour->plane.norm.y) != 1
+				&& fscanf(fd, "%lf", &currContour->plane.norm.z) != 1)
+		{
+			ERROR_PRINT("Wrong file format, "
+					"in normal to plane for contour #%d",
+					iContour);
+		}
+
+		currContour->plane.dist = 0.;
+
+		currContour->sides = new SideOfContour[currContour->ns];
+
+		for (int iSide = 0; iSide < currContour->ns; ++iSide)
+		{
+			SideOfContour* currSide = &currContour->sides[iSide];
+
+			if (fscanf(fd, "%lf", &currSide->confidence) != 1)
+			{
+				ERROR_PRINT("Wrong file format,"
+						"in confidence of side #%d of contour #%d",
+						iSide, iContour);
+				return;
+			}
+
+			if (fscanf(fd, "%d", &currSide->type) != 1)
+			{
+				ERROR_PRINT("Wrong file format,"
+						"in type of side #%d of contour #%d",
+						iSide, iContour);
+				return;
+			}
+
+			if (fscanf(fd, "%lf", &currSide->A1.x) != 1
+					&& fscanf(fd, "%lf", &currSide->A1.y) != 1
+					&& fscanf(fd, "%lf", &currSide->A1.z) != 1)
+			{
+				ERROR_PRINT("Wrong file format,"
+						"in A1 for side #%d of contour #%d",
+						iSide, iContour);
+				return;
+			}
+
+			if (fscanf(fd, "%lf", &currSide->A2.x) != 1
+					&& fscanf(fd, "%lf", &currSide->A2.y) != 1
+					&& fscanf(fd, "%lf", &currSide->A2.z) != 1)
+			{
+				ERROR_PRINT("Wrong file format,"
+						"in A1 for side #%d of contour #%d",
+						iSide, iContour);
+				return;
+			}
+		}
+	}
+
+	if (scannedString != NULL)
+	{
+		delete[] scannedString;
+		scannedString = NULL;
+	}
+}
+
+#undef STD_SC_FORMAT_HEADER_SIZE_1
+#undef STD_SC_FORMAT_HEADER_SIZE_2
