b3ca634d7dd407e29f5064dc5e2dfb7102558dd9
Ilya Palachev (home) <iliyapalachev@gmail.com>
2013-08-08 09:38:18 +0400

    [DEBUG] Remember typical testing command in script

---
+++ Scripts/global_correction_typical.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./BuildPolyhedron/Tests/globalCorrectionReal ./poly-data-in/MSS_MARQ_G_VS2_0.54_2012_09_18/polyhedron.dat ./poly-data-in/MSS_MARQ_G_VS2_0.54_2012_09_18/shadow_contours.dat cg 1e-10 1e-2
