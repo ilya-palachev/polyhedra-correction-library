43463ebdb1c5d74833d5b66c632312a00d7bf564
Ilya Palachev <iliyapalachev@gmail.com>
2013-08-12 19:09:31 +0400

    [BUGFIX] Valgrind issue in Facet_area.cpp
    ==13326== Invalid read of size 8
    ==13326==    at 0x4E4D6CA: operator-(Vector3d const&, Vector3d const&) (Vector3d.h:108)
    ==13326==    by 0x4E73122: Facet::area() (Facet_area.cpp:34)
    ==13326==    by 0x4E68BD7: GSAssociator::calculateArea(Orientation) (GSAssociator.cpp:478)
    ==13326==    by 0x4E67A00: GSAssociator::run(int, int, int) (GSAssociator.cpp:112)
    ==13326==    by 0x4E69B3D: GlobalShadeCorrector::preprocessAssociations() (GlobalShadeCorrector_preprocess.cpp:37)
    ==13326==    by 0x4E69A1D: GlobalShadeCorrector::preprocess() (GlobalShadeCorrector_preprocess.cpp:16)
    ==13326==    by 0x4E6A9A0: GlobalShadeCorrector::runCorrectionDo() (GlobalShadeCorrector.cpp:181)
    ==13326==    by 0x4E6A44E: GlobalShadeCorrector::runCorrection() (GlobalShadeCorrector.cpp:82)
    ==13326==    by 0x4E70636: Polyhedron::correctGlobal(ShadeContourData*, _GSCorrectorParameters*) (Polyhedron_correction.cpp:17)
    ==13326==    by 0x400F39: main (GlobalCorrectionReal.cpp:54)
    ==13326==  Address 0x655acc8 is 8 bytes before a block of size 960 alloc'd
    ==13326==    at 0x4C2AC27: operator new[](unsigned long) (in /usr/lib/valgrind/vgpreload_memcheck-amd64-linux.so)
    ==13326==    by 0x4E771B4: Polyhedron::Polyhedron(int, int) (Polyhedron.cpp:23)
    ==13326==    by 0x4E6783D: GSAssociator::preinit() (GSAssociator.cpp:74)
    ==13326==    by 0x4E69A78: GlobalShadeCorrector::preprocessAssociations() (GlobalShadeCorrector_preprocess.cpp:24)
    ==13326==    by 0x4E69A1D: GlobalShadeCorrector::preprocess() (GlobalShadeCorrector_preprocess.cpp:16)
    ==13326==    by 0x4E6A9A0: GlobalShadeCorrector::runCorrectionDo() (GlobalShadeCorrector.cpp:181)
    ==13326==    by 0x4E6A44E: GlobalShadeCorrector::runCorrection() (GlobalShadeCorrector.cpp:82)
    ==13326==    by 0x4E70636: Polyhedron::correctGlobal(ShadeContourData*, _GSCorrectorParameters*) (Polyhedron_correction.cpp:17)
    ==13326==    by 0x400F39: main (GlobalCorrectionReal.cpp:54)

--- Source/Polyhedron/Facet/Facet_area.cpp
+++ Source/Polyhedron/Facet/Facet_area.cpp
@@ -12,7 +12,16 @@ double Facet::area()
 	DEBUG_START;
 	my_fprint_all(stderr);
 
-	ASSERT(test_initialization());
+	bool ifInitialized = test_initialization();
+	if (!ifInitialized)
+	{
+		ERROR_PRINT("Trying to calculate area of facet, which indices have");
+		ERROR_PRINT("  been initialized yet.");
+		DEBUG_END;
+		ASSERT(ifInitialized);
+		return DEFAULT_ERROR_FOR_DOUBLE_FUNCTIONS;
+	}
+
 	Vector3d A0 = parentPolyhedron->vertices[indVertices[0]];
 	my_fprint_all(stderr);
 	DEBUG_PRINT("  in parent polyhedron there are %d vertices",
@@ -21,8 +30,20 @@ double Facet::area()
 	double areaFacet = 0.;
 	for (int iVertex = 1; iVertex < numVertices - 1; ++iVertex)
 	{
-		Vector3d A1 = parentPolyhedron->vertices[indVertices[iVertex]] - A0;
-		Vector3d A2 = parentPolyhedron->vertices[indVertices[iVertex + 1]] - A0;
+		int v0 = indVertices[iVertex];
+		int v1 = indVertices[iVertex + 1];
+		if (v0 >= parentPolyhedron->numVertices || v0 < 0 ||
+				v1 >= parentPolyhedron->numVertices || v1 < 0)
+		{
+			ERROR_PRINT("Invalid data in facet indices:");
+			ERROR_PRINT("  indVertices[%d] = %d",	iVertex, v0);
+			ERROR_PRINT("  indVertices[%d] = %d",	iVertex + 1, v1);
+			parentPolyhedron->my_fprint(stderr);
+			DEBUG_END;
+			return DEFAULT_ERROR_FOR_DOUBLE_FUNCTIONS;
+		}
+		Vector3d A1 = parentPolyhedron->vertices[v0] - A0;
+		Vector3d A2 = parentPolyhedron->vertices[v1] - A0;
 		double areaTriangle = (A1 % A2) * plane.norm * 0.5;
 		DEBUG_PRINT("\tarea of triangle # %d = %lf", iVertex - 1, areaTriangle);
 		areaFacet += areaTriangle;
