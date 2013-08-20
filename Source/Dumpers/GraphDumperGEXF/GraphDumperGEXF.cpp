3dbdb88082f3277b0b0afdecdd2228a6894f2527
Ilya Palachev <iliyapalachev@gmail.com>
2013-08-20 18:59:07 +0400

    [IMPL] Initial implementation of GraphDumperGEXF

--- Source/Dumpers/GraphDumperGEXF/GraphDumperGEXF.cpp
+++ Source/Dumpers/GraphDumperGEXF/GraphDumperGEXF.cpp
@@ -7,14 +7,77 @@
 
 #include "PolyhedraCorrectionLibrary.h"
 
-GraphDumperGEXF::GraphDumperGEXF()
+GraphDumperGEXF::GraphDumperGEXF() :
+		gexf(new libgexf::GEXF()),
+		graph(&gexf->getUndirectedGraph())
 {
-	// TODO Auto-generated constructor stub
-
+	DEBUG_START;
+	DEBUG_END;
 }
 
 GraphDumperGEXF::~GraphDumperGEXF()
 {
-	// TODO Auto-generated destructor stub
+	DEBUG_START;
+	if (gexf != NULL)
+	{
+		delete gexf;
+		gexf = NULL;
+	}
+	DEBUG_END;
 }
 
+
+void GraphDumperGEXF::addEdge(Edge* edge)
+{
+	DEBUG_START;
+
+	/* Assumption. edge->v0 < edge->v1.
+	 * This is provided by the structure of edge data. */
+
+	ASSERT(edge->v0 < edge->v1);
+
+	string v0 = to_string(edge->v0);
+	string v1 = to_string(edge->v1);
+	string id = to_string(graph->getNodeCount());
+
+	if (!graph->containsEdge(v0, v1))
+	{
+		if (!graph->containsNode(v0))
+		{
+			graph->addNode(v0);
+		}
+
+		if (!graph->containsNode(v1))
+		{
+			graph->addNode(v1);
+		}
+
+		graph->addEdge(id, v0, v1);
+	}
+
+	DEBUG_END;
+}
+
+void GraphDumperGEXF::collect(Facet* facet)
+{
+	DEBUG_START;
+
+	for (int iVertex = 0; iVertex < facet->numVertices; ++iVertex)
+	{
+		int v0 = facet->indVertices[iVertex];
+		int v1 = facet->indVertices[iVertex + 1];
+
+		if (v0 > v1)
+		{
+			int tmp = v0;
+			v0 = v1;
+			v1 = tmp;
+		}
+
+		Edge* edge = new Edge(v0, v1);
+		addEdge(edge);
+		delete edge;
+	}
+
+	DEBUG_END;
+}
--- Source/Dumpers/GraphDumperGEXF/GraphDumperGEXF.h
+++ Source/Dumpers/GraphDumperGEXF/GraphDumperGEXF.h
@@ -8,16 +8,44 @@
 #ifndef GRAPHDUMPERGEXF_H_
 #define GRAPHDUMPERGEXF_H_
 
+/* Class for dumping graph data obtained from facet/edge strcuture to file.
+ * It should be used for debugging purposes. We need to see what
+ * has been done by the algorithm with current facet.
+ *
+ * The dumper is based on LibGEXF - open library for GEXF format.
+ * See http://www.gexf.net/lib/ for details.
+ *
+ * This format has been chosen because it's used by Gephi visualizer.
+ * In future we will need to have an alternative dumper.
+ * */
+
 class GraphDumperGEXF
 {
 private:
+	/* General gexf object. */
+	libgexf::GEXF *gexf;
 
+	/* We store all data in undirected graph. */
+	libgexf::UndirectedGraph *graph;
+
+	/* Test whether the edge has already been added to the graph and add it if
+	 * not. It's assumed that edge->v0 < edge->v1 .*/
+	void addEdge(Edge* edge);
 
 public:
+
+	/* Constructor that initializes gexf and created undirected graph for
+	 * storing edges. */
 	GraphDumperGEXF();
+
+	/* Destructor that finalizes gexf. */
 	~GraphDumperGEXF();
 
+	/* Traverse through all the edges inside the facet and add all them to the
+	 * graph. */
 	void collect(Facet* facet);
+
+	/* Dump already collected graph data to the file. */
 	void dump(char* fileName);
 };
 
