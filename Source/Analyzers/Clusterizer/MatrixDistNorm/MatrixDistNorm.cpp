e9c48e17c11d138cf6065c087b1eaed313d17162
Ilya Palachev (home) <iliyapalachev@gmail.com>
2013-10-15 17:25:17 +0400

    license: Add GPLv3 license and notice to all files
    GPLv3 is contained in COPYING
    Also every file except LibGEXF/* and Vector3d/* contains copyringht and
    license notices.
    - LibGEXF can be distributed with its current license.
    - Vector3d will be rewritten and the released under GPLv3

--- Source/Analyzers/Clusterizer/MatrixDistNorm/MatrixDistNorm.cpp
+++ Source/Analyzers/Clusterizer/MatrixDistNorm/MatrixDistNorm.cpp
@@ -1,9 +1,24 @@
 /* 
- * File:   MatrixDistNorm.cpp
- * Author: nk
+ * Copyright (c) 2009-2013 Ilya Palachev <iliyapalachev@gmail.com>
+ * Copyright (c) 2009-2012 Nikolai Kaligin <nkaligin@yandex.ru>
  * 
- * Created on 1 Май 2012 г., 12:44
+ * This file is part of Polyhedra Correction Library.
+ *
+ * Polyhedra Correction Library is free software: you can redistribute 
+ * it and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Polyhedra Correction Library is distributed in the hope that it will 
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+
 #include "PolyhedraCorrectionLibrary.h"
 
 MatrixDistNorm::MatrixDistNorm() :
--- Source/DataContainers/ShadeContourData/SContour/SContour.h
+++ Source/DataContainers/ShadeContourData/SContour/SContour.h
@@ -1,8 +1,20 @@
 /* 
- * File:   SContour.h
- * Author: ilya
+ * Copyright (c) 2009-2013 Ilya Palachev <iliyapalachev@gmail.com>
+ * 
+ * This file is part of Polyhedra Correction Library.
  *
- * Created on 13 Ноябрь 2012 г., 9:50
+ * Polyhedra Correction Library is free software: you can redistribute 
+ * it and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Polyhedra Correction Library is distributed in the hope that it will 
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef SCONTOUR_H
