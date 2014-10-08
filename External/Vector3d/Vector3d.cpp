e4d065e6d93696d5c239d1fec3f6ea052397d6b3
Ilya Palachev <iliyapalachev@gmail.com>
2014-10-08 20:01:05 +0400

    [IMPL] vector3d: fix #88: clean up and migrate to fpclassify

--- External/Vector3d/Vector3d.cpp
+++ External/Vector3d/Vector3d.cpp
@@ -1,13 +1,32 @@
+/*
+ * Copyright (c) 2009-2014 Ilya Palachev <iliyapalachev@gmail.com>
+ *
+ * This file is part of Polyhedra Correction Library.
+ *
+ * Polyhedra Correction Library is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Polyhedra Correction Library is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Polyhedra Correction Library.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <math.h>
 
 #include "Vector3d.h"
 #include "Gauss_string.h"
 
-Vector3d & Vector3d::norm(double d)
+Vector3d &Vector3d::norm(double d)
 {
 	double t = x * x + y * y + z * z;
-	//    if ( t )
-	if (_nequal(t, 0))
+	if (fpclassify(t) = FP_ZERO)
 	{
 		d /= sqrt(t);
 		x *= d;
@@ -17,48 +36,12 @@ Vector3d & Vector3d::norm(double d)
 	return *this;
 }
 
-Vector3d Vector3d::perpendicular() const
-{
-	double ax = fabs(x);
-	double ay = fabs(y);
-	double az = fabs(z);
-	if (ax >= ay)
-	{
-		if (ax >= az)
-		{
-			//            if ( ! ax ) return Vector3d (1.,0.,0.);
-			if (_equal(ax, 0))
-				return Vector3d(1., 0., 0.);
-			double d = 1. / sqrt(ax * ax + ay * ay);
-			return Vector3d(-y * d, x * d, 0.);
-		}
-	}
-	else
-	{
-		if (ay >= az)
-		{
-			double d = 1. / sqrt(ay * ay + az * az);
-			return Vector3d(0., -z * d, y * d);
-		}
-	}
-	double d = 1. / sqrt(ax * ax + az * az);
-	return Vector3d(z * d, 0., -x * d);
-}
-
-double length(const Vector3d & v)
+double length(const Vector3d &v)
 {
 	return sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
 }
 
-void reper(const Vector3d & x, Vector3d & y, Vector3d & z)
-{
-	y = x.perpendicular();
-	z = x % y;
-}
-
-///////////////////////////////////////////////////////////////////////////////
-
-Plane::Plane(const Vector3d & x, const Vector3d & y, const Vector3d & z)
+Plane::Plane(const Vector3d &x, const Vector3d &y, const Vector3d &z)
 {
 	const Vector3d xy = x - y;
 	const double d1 = qmod(xy);
@@ -85,69 +68,18 @@ Plane::Plane(const Vector3d & x, const Vector3d & y, const Vector3d & z)
 	dist = ((x + y + z) * norm) / -3.;
 }
 
-// �������� ����� �� ���������
-
-Vector3d Plane::project(const Vector3d & v) const
+Vector3d Plane::project(const Vector3d &v) const
 {
 	return v - (*this % v) * norm;
 }
 
-// ����������� ���� ����������
-
-//bool intersection(const Plane & plane1, const Plane & plane2, const Plane & plane3, Vector3d & point) {
-//    const double d =
-//            plane1.norm.x * (plane2.norm.y * plane3.norm.z - plane3.norm.y * plane2.norm.z) +
-//            plane2.norm.x * (plane3.norm.y * plane1.norm.z - plane1.norm.y * plane3.norm.z) +
-//            plane3.norm.x * (plane1.norm.y * plane2.norm.z - plane2.norm.y * plane1.norm.z);
-//
-//    //    if ( d == 0 ) return false;
-//    if (_equal(d, 0)) return false;
-//
-//    point.x =
-//            plane1.dist * (plane2.norm.y * plane3.norm.z - plane3.norm.y * plane2.norm.z) +
-//            plane2.dist * (plane3.norm.y * plane1.norm.z - plane1.norm.y * plane3.norm.z) +
-//            plane3.dist * (plane1.norm.y * plane2.norm.z - plane2.norm.y * plane1.norm.z);
-//
-//    point.y =
-//            plane1.norm.x * (plane2.dist * plane3.norm.z - plane3.dist * plane2.norm.z) +
-//            plane2.norm.x * (plane3.dist * plane1.norm.z - plane1.dist * plane3.norm.z) +
-//            plane3.norm.x * (plane1.dist * plane2.norm.z - plane2.dist * plane1.norm.z);
-//
-//    point.z =
-//            plane1.norm.x * (plane2.norm.y * plane3.dist - plane3.norm.y * plane2.dist) +
-//            plane2.norm.x * (plane3.norm.y * plane1.dist - plane1.norm.y * plane3.dist) +
-//            plane3.norm.x * (plane1.norm.y * plane2.dist - plane2.norm.y * plane1.dist);
-//
-//    point *= -1. / d;
-//    return true;
-//}
-
-//bool intersection(const Plane & plane1, const Plane & plane2, const Plane & plane3, Vector3d & point) {
-//    A = Matrix(3, 3);
-//    A(1, 1) = plane1.norm.x;
-//    A(1, 2) = plane1.norm.y;
-//    A(1, 3) = plane1.norm.z;
-//    A(2, 1) = plane2.norm.x;
-//    A(2, 2) = plane2.norm.y;
-//    A(2, 3) = plane2.norm.z;
-//    A(3, 1) = plane3.norm.x;
-//    A(3, 2) = plane3.norm.y;
-//    A(3, 3) = plane3.norm.z;
-//    B = Matrix(3, 1);
-//    B(1, 1) = - plane1.dist;
-//    B(2, 1) = - plane2.dist;
-//    B(3, 1) = - plane3.dist;
-//    B = B / A;
-//    printf("%lf, %lf, %lf\n", B(1, 1), B(2, 1), B(3, 1));
-//}
-bool intersection(const Plane & plane1, const Plane & plane2,
-		const Plane & plane3, Vector3d & point)
+bool intersection(const Plane &plane1, const Plane &plane2,
+		const Plane &plane3, Vector3d &point)
 {
-	double *A, *b/*, *INV*/;
+	double *A, *b;
 	bool ret;
 	A = new double[9];
 	b = new double[3];
-//    INV = new double[9];
 	A[0] = plane1.norm.x;
 	A[1] = plane1.norm.y;
 	A[2] = plane1.norm.z;
@@ -160,11 +92,7 @@ bool intersection(const Plane & plane1, const Plane & plane2,
 	b[0] = -plane1.dist;
 	b[1] = -plane2.dist;
 	b[2] = -plane3.dist;
-//    invert(A, INV, 3);
 	ret = Gauss_string(3, A, b);
-//    point.x = b[0] * A[0] + b[1] * A[1] + b[2] * A[2];
-//    point.y = b[0] * A[3] + b[1] * A[4] + b[2] * A[5];
-//    point.z = b[0] * A[6] + b[1] * A[7] + b[2] * A[8];
 	point.x = b[0];
 	point.y = b[1];
 	point.z = b[2];
@@ -182,9 +110,8 @@ bool intersection(const Plane & plane1, const Plane & plane2,
 	return ret;
 }
 
-// ����������� ���� ����������
-bool intersection(const Plane & plane1, const Plane & plane2, Vector3d & dir,
-		Vector3d & point)
+bool intersection(const Plane &plane1, const Plane &plane2, Vector3d &dir,
+		Vector3d &point)
 {
 	const Vector3d v = plane1.norm % plane2.norm;
 	const double s = v * v;
@@ -255,8 +182,6 @@ bool intersection(const Plane & plane1, const Plane & plane2, Vector3d & dir,
 	return true;
 }
 
-// ����������� ��������� � ������
-
 bool intersection(const Plane & plane, const Vector3d & dir,
 		const Vector3d & point, Vector3d & res)
 {
@@ -268,198 +193,4 @@ bool intersection(const Plane & plane, const Vector3d & dir,
 	res = point - dir * ((plane % point) / s);
 	return true;
 }
-
-// ��������� ���������� ����� �������
-
-bool cut(const Plane & plane, Segment3d & seg)
-{
-	const double a = plane % seg.a;
-	const double b = plane % seg.b;
-	if (a <= 0)
-	{
-		if (b > 0)
-		{
-			const double c = b - a;
-			seg.b = seg.a * (b / c) - seg.b * (a / c);
-		}
-	}
-	else
-	{
-		if (b > 0)
-			return false;
-		const double c = b - a;
-		seg.a = seg.a * (b / c) - seg.b * (a / c);
-	}
-	return true;
-}
-
-///////////////////////////////////////////////////////////////////////////////
-
-Spin::Spin(Vector3d axis, double angle)
-{
-	if (!axis)
-	{
-		t = 1.;
-		x = y = z = 0.;
-	}
-	else
-	{
-		angle *= 0.5;
-		t = cos(angle);
-		axis.norm(sin(angle));
-		x = axis.x;
-		y = axis.y;
-		z = axis.z;
-	}
-}
-
-Spin::Spin(Vector3d v1, Vector3d v2)
-{
-	t = v1.norm() * v2.norm();
-	if (!v1 || !v2 || t >= 1.)
-	{
-		t = 1.;
-		x = y = z = 0.;
-		return;
-	}
-	t += 1.;
-	Vector3d v = v1 % v2;
-	double d = t * t + v * v;
-	if (t <= 0 || d < 1e-18)
-	{
-		v = v1.perpendicular();
-		t = 0.;
-		x = v.x;
-		y = v.y;
-		z = v.z;
-		return;
-	}
-	d = 1. / sqrt(d);
-	t *= d;
-	x = v.x * d;
-	y = v.y * d;
-	z = v.z * d;
-}
-
-Spin::Spin(const Vector3d & vx, const Vector3d & vy, const Vector3d & vz)
-{
-	double la = 0.25 * (1. + vx.x - vy.y - vz.z);
-	double mu = 0.25 * (1. - vx.x + vy.y - vz.z);
-	double nu = 0.25 * (1. - vx.x - vy.y + vz.z);
-	double ro = 0.25 * (1. + vx.x + vy.y + vz.z);
-	la = (la <= 0) ? 0. : sqrt(la);
-	mu = (mu <= 0) ? 0. : sqrt(mu);
-	nu = (nu <= 0) ? 0. : sqrt(nu);
-	ro = (ro <= 0) ? 0. : sqrt(ro);
-	if (la >= mu)
-		if (la >= nu)
-		{
-			if (vx.y + vy.x < 0)
-				mu = -mu;
-			if (vy.z - vz.y < 0)
-				ro = -ro;
-			if (vz.x + vx.z < 0)
-				nu = -nu;
-		}
-		else
-		{
-			if (vx.y - vy.x < 0)
-				ro = -ro;
-			if (vy.z + vz.y < 0)
-				mu = -mu;
-			if (vz.x + vx.z < 0)
-				la = -la;
-		}
-	else if (nu >= mu)
-	{
-		if (vx.y - vy.x < 0)
-			ro = -ro;
-		if (vy.z + vz.y < 0)
-			mu = -mu;
-		if (vz.x + vx.z < 0)
-			la = -la;
-	}
-	else
-	{
-		if (vx.y + vy.x < 0)
-			la = -la;
-		if (vy.z + vz.y < 0)
-			nu = -nu;
-		if (vz.x - vx.z < 0)
-			ro = -ro;
-	}
-	x = la;
-	y = mu;
-	z = nu;
-	t = ro;
-}
-
-Spin & Spin::operator *=(const Spin & s)
-{
-	double st = t * s.t - x * s.x - y * s.y - z * s.z;
-	double sx = t * s.x + x * s.t + y * s.z - z * s.y;
-	double sy = t * s.y + y * s.t + z * s.x - x * s.z;
-	double sz = t * s.z + z * s.t + x * s.y - y * s.x;
-	double d = st * st + sx * sx + sy * sy + sz * sz;
-	if (fabs(d - 1.) > 1e-14)
-	{
-		d = 1. / sqrt(d);
-		t = st * d;
-		x = sx * d;
-		y = sy * d;
-		z = sz * d;
-	}
-	else
-	{
-		t = st;
-		x = sx;
-		y = sy;
-		z = sz;
-	}
-	return *this;
-}
-
-Vector3d Spin::getAxis() const
-{
-	//    if ( !x && !y && !z )
-	if (_equal(x, 0) && _equal(y, 0) && _equal(z, 0))
-	{
-		return Vector3d(1., 0., 0.);
-	}
-	else
-	{
-		Vector3d v(x, y, z);
-		return v.norm();
-	}
-}
-
-double Spin::getAngle() const
-{
-	return fabs(t) >= 1. ? 0. : 2. * acos(t);
-}
-
-void Spin::getReper(Vector3d & ax, Vector3d & ay, Vector3d & az) const
-{
-	const double xx = x * x;
-	const double yy = y * y;
-	const double zz = z * z;
-	const double tt = t * t;
-	const double xy = x * y;
-	const double yz = y * z;
-	const double zx = z * x;
-	const double xt = x * t;
-	const double yt = y * t;
-	const double zt = z * t;
-	ax.x = tt + xx - yy - zz;
-	ax.y = 2. * (xy - zt);
-	ax.z = 2. * (zx + yt);
-	ay.x = 2. * (xy + zt);
-	ay.y = tt + yy - zz - xx;
-	ay.z = 2. * (yz - xt);
-	az.x = 2. * (zx - yt);
-	az.y = 2. * (yz + xt);
-	az.z = tt + zz - xx - yy;
-}
-
-///////////////////////////////////////////////////////////////////////////////
 
--- External/Vector3d/Vector3d.h
+++ External/Vector3d/Vector3d.h
@@ -1,47 +1,72 @@
+/*
+ * Copyright (c) 2009-2014 Ilya Palachev <iliyapalachev@gmail.com>
+ *
+ * This file is part of Polyhedra Correction Library.
+ *
+ * Polyhedra Correction Library is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Polyhedra Correction Library is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Polyhedra Correction Library.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file Vector3d.h
+ * @brief 3D vectors and planes declaration, with their intersections.
+ */
+
 #ifndef VECTOR3D_H
 #define VECTOR3D_H
 
-#define GCC
-
-#ifdef  GCC
-
-#ifndef _equal
-#define _equal(a,b)   ((a)<=(b)&&(a)>=(b))
-#define _nequal(a,b)  ((a)<(b)||(a)>(b))
-#endif  // _equal
-#else
-
-#define _equal(a,b)   ((a)==(b))
-#define _nequal(a,b)  ((a)!=(b))
-
-#endif // GCC
+/** Simple 3D vector class.*/
 class Vector3d
 {
 public:
-	double x, y, z;
+	double x; /**< The X coordinate. */
+       	double y; /**< The Y coordinate. */
+        double z; /**< The Z coordinate. */
 
+	/** Empty constructor. */
 	Vector3d() :
-					x(0),
-					y(0),
-					z(0)
-	{
-	}
-	Vector3d(double a, double b, double c) :
-					x(a),
-					y(b),
-					z(c)
+		x(0.),
+		y(0.),
+		z(0.)
 	{
 	}
 
-	/* Added by Ilya Palachev on 2013-04-24
-	 * to make possible some tricks with associator
-	 * in Polyhedron correction
+	/**
+	 * Constructor by coordinates.
+	 *
+	 * @param a	The X coordinate.
+	 * @param b	The Y coordinate.
+	 * @param c	The Z coordinate.
 	 */
-	Vector3d& operator =(const Vector3d& orig)
+	Vector3d(double a, double b, double c) :
+		x(a),
+		y(b),
+		z(c)
 	{
-		x = orig.x;
-		y = orig.y;
-		z = orig.z;
+	}
+
+	/**
+	 * Simple assignment operator
+	 *
+	 * @param v	The vector that should be assigned from.
+	 * @return The result of the assignment.
+	 */
+	Vector3d& operator =(const Vector3d& v)
+	{
+		x = v.x;
+		y = v.y;
+		z = v.z;
 		return *this;
 	}
 
@@ -49,6 +74,7 @@ public:
 	 * Compares 2 vectors in lexicographical order.
 	 *
 	 * @param v	Reference to 2nd vector
+	 * @return	The result of the comparison.
 	 */
 	bool operator <(const Vector3d& v) const
 	{
@@ -64,6 +90,12 @@ public:
 		return false;
 	}
 
+	/**
+	 * Compares 2 vectors in lexicographical order.
+	 *
+	 * @param v	Reference to 2nd vector
+	 * @return	The result of the comparison.
+	 */
 	bool operator >(const Vector3d& v) const
 	{
 		if (x > v.x)
@@ -78,16 +110,29 @@ public:
 		return false;
 	}
 
+	/**
+	 * Compares 2 vectors in lexicographical order.
+	 *
+	 * @param v	Reference to 2nd vector
+	 * @return	The result of the comparison.
+	 */
 	bool operator >=(const Vector3d& v) const
 	{
 		return !(this->operator<(v));
 	}
 
+	/**
+	 * Compares 2 vectors in lexicographical order.
+	 *
+	 * @param v	Reference to 2nd vector
+	 * @return	The result of the comparison.
+	 */
 	bool operator <=(const Vector3d& v) const
 	{
 		return !(this->operator>(v));
 	}
 
+	/* TODO: complete comments for all functions. */
 	Vector3d operator -() const
 	{
 		return Vector3d(-x, -y, -z);
@@ -133,12 +178,12 @@ public:
 
 	bool operator !() const
 	{
-//        return !x && !y && !z;
-		return _equal(x,0) && _equal(y,0) && _equal(z,0);
+		return fpclassify(x) == FP_ZERO &&
+			fpclassify(y) == FP_ZERO &&
+			fpclassify(z) == FP_ZERO;
 	}
 
-	Vector3d & norm(double d = 1.);
-	Vector3d perpendicular() const;
+	Vector3d& norm(double d);
 };
 
 inline Vector3d operator +(const Vector3d& a, const Vector3d& b)
@@ -184,36 +229,15 @@ inline Vector3d operator %(const Vector3d& a, const Vector3d& b)
 
 inline bool operator !=(const Vector3d& a, const Vector3d& b)
 {
-//    return a.x != b.x || a.y != b.y || a.z != b.z;
 	return _nequal(a.x,b.x) || _nequal(a.y,b.y) || _nequal(a.z,b.z);
 }
 
 inline bool operator ==(const Vector3d& a, const Vector3d& b)
 {
-//    return a.x == b.x && a.y == b.y && a.z == b.z;
 	return _equal(a.x,b.x) && _equal(a.y,b.y) && _equal(a.z,b.z);
 }
 
-void reper(const Vector3d & x, Vector3d & y, Vector3d & z);
-
 double length(const Vector3d & v);
-
-///////////////////////////////////////////////////////////////////////////////
-
-class Segment3d
-{
-public:
-	Vector3d a, b;
-
-	Segment3d()
-	{
-	}
-	Segment3d(const Vector3d & va, const Vector3d & vb) :
-					a(va),
-					b(vb)
-	{
-	}
-};
 
 inline bool operator !=(const Segment3d & s1, const Segment3d & s2)
 {
@@ -230,8 +254,6 @@ inline double length(const Segment3d & s)
 	return length(s.b - s.a);
 }
 
-///////////////////////////////////////////////////////////////////////////////
-
 class Plane
 {
 public:
@@ -239,16 +261,20 @@ public:
 	double dist;
 
 	Plane() :
-					dist(0)
+		norm(Vector3d(0., 0., 0.));
+		dist(0.)
 	{
 	}
+
 	Plane(const Vector3d &, const Vector3d &, const Vector3d &);
-	Plane(const Vector3d & v, const double & d) :
-					norm(v),
-					dist(d)
+
+	Plane(const Vector3d &v, const double &d) :
+		norm(v),
+		dist(d)
 	{
 	}
-	double operator %(const Vector3d & v) const
+
+	double operator %(const Vector3d &v) const
 	{
 		return norm.x * v.x + norm.y * v.y + norm.z * v.z + dist;
 	}
@@ -258,230 +284,27 @@ public:
 		return Plane(-norm, -dist);
 	}
 
-	Vector3d project(const Vector3d & v) const; // �������� ����� �� ���������
+	Vector3d project(const Vector3d &v) const;
 };
 
-inline bool operator ==(const Plane & p1, const Plane & p2)
+inline bool operator ==(const Plane &p1, const Plane &p2)
 {
-//    return p1.norm == p2.norm && p1.dist == p2.dist;
-	return p1.norm == p2.norm && _equal(p1.dist,p2.dist);
+	return p1.norm == p2.norm && _equal(p1.dist, p2.dist);
 }
 
-inline bool operator !=(const Plane & p1, const Plane & p2)
+inline bool operator !=(const Plane &p1, const Plane &p2)
 {
 	return p1.norm != p2.norm || _nequal(p1.dist,p2.dist);
 }
 
-// ����������� ���� ����������
-bool intersection(const Plane & plane1, const Plane & plane2,
-		const Plane & plane3, Vector3d & point);
-// ����������� ���� ����������
-bool intersection(const Plane & plane1, const Plane & plane2, Vector3d & dir,
-		Vector3d & point);
-// ����������� ��������� � ������
-bool intersection(const Plane & plane, const Vector3d & dir,
-		const Vector3d & point, Vector3d & res);
-// ��������� ���������� ����� �������
-bool cut(const Plane & plane, Segment3d & seg);
+bool intersection(const Plane &plane1, const Plane &plane2,
+		const Plane &plane3, Vector3d &point);
 
-///////////////////////////////////////////////////////////////////////////////
+bool intersection(const Plane &plane1, const Plane &plane2, Vector3d &dir,
+		Vector3d &point);
 
-class Spin
-{
-	double t, x, y, z;
-public:
-	Spin()
-	{
-		t = 1.;
-		x = y = z = 0.;
-	}
-	Spin(Vector3d, double);
-	Spin(Vector3d v1, Vector3d v2);
-	Spin(const Vector3d & x, const Vector3d & y, const Vector3d & z);
-	void getReper(Vector3d & ax, Vector3d & ay, Vector3d & az) const;
-	double getAngle() const;
-	Vector3d getAxis() const;
-	Vector3d operator()(const Vector3d & v) const
-	{
-		Vector3d ax, ay, az;
-		getReper(ax, ay, az);
-		return Vector3d(v * ax, v * ay, v * az);
-	}
-	Spin & operator*=(const Spin &);
-	Spin operator ~() const
-	{
-		Spin s = *this;
-		s.t = -t;
-		return s;
-	}
-};
+bool intersection(const Plane &plane, const Vector3d &dir,
+		const Vector3d &point, Vector3d &res);
 
-inline Spin operator *(const Spin & s1, const Spin & s2)
-{
-	Spin s = s1;
-	return s *= s2;
-}
-
-///////////////////////////////////////////////////////////////////////////////
-
-class Conform
-{
-public:
-	Spin spin;
-	Vector3d trans;
-	double magn;
-
-	Conform() :
-					trans(0., 0., 0.),
-					magn(1.)
-	{
-	}
-
-	explicit Conform(const Spin & s) :
-					spin(s),
-					trans(0., 0., 0.),
-					magn(1.)
-	{
-	}
-
-	Conform(const Spin & s, const Vector3d & t, double m = 1.) :
-					spin(s),
-					trans(t),
-					magn(m)
-	{
-	}
-
-	Conform operator~() const
-	{
-		const Spin s(~spin);
-		const double m = 1. / magn;
-		return Conform(s, (-m) * s(trans), m);
-	}
-
-	Plane operator()(const Plane & p) const
-	{
-		const Vector3d v = spin(p.norm);
-		return Plane(v, magn * p.dist - v * trans);
-	}
-
-	Vector3d operator()(const Vector3d & v) const
-	{
-		return spin(v) * magn + trans;
-	}
-
-	Conform & operator*=(const Conform & c)
-	{
-		trans += spin(c.trans) * magn;
-		spin *= c.spin;
-		magn *= c.magn;
-		return *this;
-	}
-
-};
-
-inline Conform operator *(const Conform & c1, const Conform & c2)
-{
-	Conform c = c1;
-	return c *= c2;
-}
-
-///////////////////////////////////////////////////////////////////////////////
-
-class Similarity
-{
-	double magn;
-	Vector3d ox, oy, oz;
-	Vector3d ax, ay, az, trans;
-public:
-	explicit Similarity(const Spin & spin) :
-					magn(1.),
-					trans(0., 0., 0.)
-	{
-		spin.getReper(ox, oy, oz);
-		ax = ox;
-		ay = oy;
-		az = oz;
-	}
-
-	explicit Similarity(const Conform & c) :
-					magn(c.magn),
-					trans(c.trans)
-	{
-		c.spin.getReper(ox, oy, oz);
-		ax = ox * magn;
-		ay = oy * magn;
-		az = oz * magn;
-	}
-
-	Plane operator()(const Plane & p) const
-	{
-		const Vector3d v(ox * p.norm, oy * p.norm, oz * p.norm);
-		return Plane(v, magn * p.dist - v * trans);
-	}
-
-	Vector3d operator ()(const Vector3d & p) const
-	{
-		return Vector3d(ax * p + trans.x, ay * p + trans.y, az * p + trans.z);
-	}
-
-	double getX(const Vector3d & p) const
-	{
-		return ax * p + trans.x;
-	}
-
-	double getY(const Vector3d & p) const
-	{
-		return ay * p + trans.y;
-	}
-
-	double getZ(const Vector3d & p) const
-	{
-		return az * p + trans.z;
-	}
-};
-
-//**************************** 14.07.2008 ****************************
-//
-//                     �������������� ��������
-//
-//**************************** 14.07.2008 ****************************
-
-inline Vector3d & operator *=(Vector3d & v, const Similarity & sim)
-{
-	return v = sim(v);
-}
-
-inline Vector3d & operator *=(Vector3d & v, const Conform & conf)
-{
-	return v = conf(v);
-}
-
-inline Vector3d & operator *=(Vector3d & v, const Spin & spin)
-{
-	return v = spin(v);
-}
-
-//**************************** 14.07.2008 ****************************
-//
-//                     �������������� ���������
-//
-//**************************** 14.07.2008 ****************************
-
-inline Plane & operator *=(Plane & plane, const Similarity & sim)
-{
-	return plane = sim(plane);
-}
-
-inline Plane & operator *=(Plane & plane, const Conform & conf)
-{
-	return plane = conf(plane);
-}
-
-inline Plane & operator *=(Plane & plane, const Spin & spin)
-{
-	plane.norm = spin(plane.norm);
-	return plane;
-}
-
-#endif
+#endif /* VECTOR3D_H */
 
