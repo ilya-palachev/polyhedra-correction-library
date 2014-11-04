7cccc5871290da044c34b66cff61947ce7143484
Ilya Palachev <iliyapalachev@gmail.com>
2014-11-04 15:02:30 +0300

    [REFACTOR] cgal: remove trailing spaces

--- External/CGAL/halfspaces_intersection.h
+++ External/CGAL/halfspaces_intersection.h
@@ -11,15 +11,15 @@ namespace internal
   template <class Polyhedron>
   class Build_dual_polyhedron :
     public CGAL::Modifier_base<typename Polyhedron::HalfedgeDS>
-  { 
+  {
     typedef typename Polyhedron::HalfedgeDS HDS;
     const Polyhedron &_primal;
-      
+
   public:
     Build_dual_polyhedron (const Polyhedron & primal):
       _primal (primal)
     {}
-      
+
     void operator () (HDS &hds)
     {
       typedef typename Polyhedron::Facet Facet;
@@ -27,11 +27,11 @@ namespace internal
       typedef typename Polyhedron::Facet_const_iterator Facet_const_iterator;
       typedef typename Polyhedron::Vertex_const_iterator Vertex_const_iterator;
       typename CGAL::Polyhedron_incremental_builder_3<HDS> B( hds, true);
-      
+
       B.begin_surface(_primal.size_of_facets(),
 		      _primal.size_of_vertices(),
 		      _primal.size_of_halfedges());
-      
+
       // compute coordinates of extreme vertices in the dual polyhedron
       // from primal faces
       std::map<Facet_const_handle, size_t> extreme_points;
@@ -47,7 +47,7 @@ namespace internal
 	  B.add_vertex(CGAL::ORIGIN + p.orthogonal_vector () / (-p.d()));
 	  extreme_points[it] = n;
 	}
-      
+
       // build faces
       for (Vertex_const_iterator it = _primal.vertices_begin();
 	   it != _primal.vertices_end(); ++it)
@@ -83,7 +83,7 @@ namespace internal
       dual_points.push_back(CGAL::ORIGIN + p->orthogonal_vector () / (-p->d()));
 
     Polyhedron ch;
-    CGAL::convex_hull_3(dual_points.begin(), dual_points.end(), ch);    
+    CGAL::convex_hull_3(dual_points.begin(), dual_points.end(), ch);
 
     Builder build_dual (ch);
     P.delegate(build_dual);
