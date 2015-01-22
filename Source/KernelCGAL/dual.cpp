6ee23551a555c72653d9c18e34f4997d975ed5c7
Ilya Palachev <iliyapalachev@gmail.com>
2015-01-22 23:38:51 +0300

    [IMPL] kernel-cgal: add duality transformation

---
+++ Source/KernelCGAL/dual.cpp
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2009-2014 Ilya Palachev <iliyapalachev@gmail.com>
+ *
+ * This file is part of Polyhedra Correction Library.
+ *
+ * Polyhedra Correction Library is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Polyhedra Correction Library is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Polyhedra Correction Library.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file dual.cpp
+ * @brief Transformation of duality between planes and points (implementation)
+ */
+
+#include "KernelCGAL/dual.h"
+
+Point_3 dual(Plane_3 p)
+{
+	double reciprocal = -1. / p.d();
+	return Point_3(p.a() * reciprocal, p.b() * reciprocal,
+			p.c() * reciprocal);
+}
+
+Plane_3 dual(Point_3 p)
+{
+	return Plane_3(p.x(), p.y(), p.z(), -1.);
+}
---
+++ Source/KernelCGAL/dual.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2009-2014 Ilya Palachev <iliyapalachev@gmail.com>
+ *
+ * This file is part of Polyhedra Correction Library.
+ *
+ * Polyhedra Correction Library is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Polyhedra Correction Library is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Polyhedra Correction Library.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file dual.h
+ * @brief Transformation of duality between planes and points.
+ */
+
+#ifndef KERNELCGAL_DUAL_H
+#define KERNELCGAL_DUAL_H
+
+#include "KernelCGAL/KernelCGAL.h"
+
+/**
+ * Creates a dual point for a given plane.
+ *
+ * @param p	The plane.
+ * @return	The dual point.
+ */
+Point_3 dual(Plane_3 p);
+
+/**
+ * Calculates dual plane for a given point.
+ *
+ * @param p	The point.
+ * @return	The dual plane.
+ */
+Plane_3 dual(Point_3 p);
+
+#endif /* KERNELCGAL_DUAL_H */
