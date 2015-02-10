26495369fb0f46c18cd33ac5877b6263d0f3978c
Ilya Palachev <iliyapalachev@gmail.com>
2015-02-10 20:48:54 +0300

    [DEBUG] sfed-ctor: remove dump of support matrix

--- Source/DataConstructors/SupportFunctionEstimationDataConstructor/SupportFunctionEstimationDataConstructor.cpp
+++ Source/DataConstructors/SupportFunctionEstimationDataConstructor/SupportFunctionEstimationDataConstructor.cpp
@@ -198,8 +198,6 @@ static SupportMatrix *buildSupportMatrix(SupportFunctionDataPtr data,
 	}
 	if (!matrix)
 		exit(EXIT_FAILURE);
-	globalPCLDumper(PCL_DUMPER_LEVEL_DEBUG, "support-matrix.mat")
-		<< *matrix;
 	DEBUG_END;
 	return matrix;
 }
