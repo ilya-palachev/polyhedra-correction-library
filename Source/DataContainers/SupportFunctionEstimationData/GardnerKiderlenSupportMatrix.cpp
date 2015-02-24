9ac207d02bd4b7d94abcee5e25b3a364bb2f576f
Ilya Palachev <iliyapalachev@gmail.com>
2015-02-24 21:05:20 +0300

    [IMPL] gk-sm: add epsilon-factor static member
    Signed-off-by: Ilya Palachev <iliyapalachev@gmail.com>

--- Source/DataContainers/SupportFunctionEstimationData/GardnerKiderlenSupportMatrix.cpp
+++ Source/DataContainers/SupportFunctionEstimationData/GardnerKiderlenSupportMatrix.cpp
@@ -147,13 +147,14 @@ static std::vector<Point> getShiftedDualPoints(SupportFunctionDataPtr data,
 	return points;
 }
 
+double GardnerKiderlenSupportMatrix::epsilonFactor = 1.;
 
 GardnerKiderlenSupportMatrix *constructReducedGardnerKiderlenSupportMatrix(
 		SupportFunctionDataPtr data, double epsilon)
 {
 	DEBUG_START;
 	std::cout << "epsilon = " << epsilon << std::endl;
-	std::cin >> epsilon;
+	epsilon *= GardnerKiderlenSupportMatrix::epsilonFactor;
 
 	/* Get duals of higher and lower support planes */
 	auto pointsHigher = getShiftedDualPoints(data, epsilon);
