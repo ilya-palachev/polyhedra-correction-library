4a47153a0b66f34bcd77056fda80cf5ca0c07180
Ilya Palachev <iliyapalachev@gmail.com>
2016-06-15 21:26:08 +0300

    [IMPL] join-2d-facets: continue implementation
    Signed-off-by: Ilya Palachev <iliyapalachev@gmail.com>

--- Tests/join2DFacets.cpp
+++ Tests/join2DFacets.cpp
@@ -28,6 +28,9 @@
 #include <sys/time.h>
 #include <unistd.h>
 #include <cstdio>
+#include <set>
+#include <vector>
+#include <Eigen/LU>
 
 #include <CGAL/Cartesian.h>
 typedef CGAL::Cartesian<double> K;
@@ -72,7 +75,6 @@ static double genRandomDouble(double maxDelta)
 std::vector<Point_2> generateExtremePoints(int numPoints)
 {
 	std::vector<Point_2> points;
-#if 0
 	for (int i = 0; i < numPoints; ++i)
 	{
 		do
@@ -89,7 +91,7 @@ std::vector<Point_2> generateExtremePoints(int numPoints)
 		}
 		while (1);
 	}
-#endif
+#if 0
 	double maxDelta = 2. / ((double) numPoints);
 	for (int i = 0; i < numPoints; ++i)
 	{
@@ -103,6 +105,7 @@ std::vector<Point_2> generateExtremePoints(int numPoints)
 	convex_hull_2(points.begin(), points.end(), std::back_inserter(hull));
 	assert(hull.size() == numPoints);
 	std::cout << "Number of points in the hull: " << hull.size() << std::endl;
+#endif
 	return points;
 }
 
@@ -152,11 +155,245 @@ prepareSupportData(std::vector<Point_2> points)
 	return std::make_pair(directions, values);
 }
 
+std::vector<std::set<int>> prepareInitialClusters(int numPoints,
+		int numClusters)
+{
+	std::vector<std::set<int>> clusters(numClusters);
+	double step = ((double) numPoints) / ((double) numClusters);
+	double bound = 0.;
+	int iPoint = 0;
+	int iCluster = 0;
+	for (auto &cluster : clusters)
+	{
+		bound += step;
+		while (iPoint < bound && iPoint < numPoints)
+			cluster.insert(iPoint++);
+		++iCluster;
+	}
+	std::cout << "Initial clusters:" << std::endl;
+	for (const auto &cluster : clusters)
+	{
+		std::cout << "cluster: ";
+		for (const int i : cluster)
+			std::cout << i << " ";
+		std::cout << std::endl;
+		if (cluster.empty())
+		{
+			std::cerr << "Cluster is empty!" << std::endl;
+			exit(EXIT_FAILURE);
+		}
+	}
+	return clusters;
+}
+
+std::pair<Point_2, double> calculateClusterErrorAndCenter(
+		const std::vector<Vector_2> &directions,
+		const std::vector<double> &values,
+		const std::set<int> &cluster)
+{
+	Eigen::Matrix2d A;
+	Eigen::Vector2d b;
+	for (int i = 0; i < 2; ++i)
+	{
+		for (int j = 0; j < 2; ++j)
+			A(i, j) = 0.;
+		b(i) = 0.;
+	}
+
+	for (const int &i : cluster)
+	{
+		double u[2];
+		u[0] = directions[i].x();
+		u[1] = directions[i].y();
+		double h = values[i];
+
+		for (int i = 0; i < 2; ++i)
+		{
+			for (int j = 0; j < 2; ++j)
+				A(i, j) += u[i] * u[j];
+			b(i) += u[i] * h;
+		}
+	}
+
+	Eigen::Matrix2d inverse = A.inverse();
+	Eigen::Vector2d x = inverse * b;
+	
+	double error = 0.;
+	for (const int &i : cluster)
+	{
+		double u[2];
+		u[0] = directions[i].x();
+		u[1] = directions[i].y();
+		double h = values[i];
+		double diff = u[0] * x(0) + u[1] * x[1] - h;
+		error += diff * diff;
+	}
+	Point_2 center(x(0), x(1));
+	return std::make_pair(center, error);
+}
+
+double calculateClusterError(
+		const std::vector<Vector_2> &directions,
+		const std::vector<double> &values,
+		const std::set<int> &cluster)
+{
+	return calculateClusterErrorAndCenter(
+			directions, values,
+			cluster).second;
+}
+
+void exchangeItemToRight(std::set<int> &cluster, std::set<int> &clusterNext)
+{
+	auto itLast = cluster.end();
+	--itLast;
+	clusterNext.insert(*itLast);
+	cluster.erase(itLast);
+}
+
+void exchangeItemToRight(std::vector<std::set<int>> &clusters, int iCluster)
+{
+	int numClusters = clusters.size();
+	int iClusterNext = (numClusters + iCluster + 1) % numClusters;
+	exchangeItemToRight(clusters[iCluster], clusters[iClusterNext]);
+}
+
+void exchangeItemToLeft(std::set<int> &cluster, std::set<int> &clusterNext)
+{
+	auto itFirst = clusterNext.begin();
+	cluster.insert(*itFirst);
+	clusterNext.erase(itFirst);
+}
+
+void exchangeItemToLeft(std::vector<std::set<int>> &clusters, int iCluster)
+{
+	int numClusters = clusters.size();
+	int iClusterNext = (numClusters + iCluster + 1) % numClusters;
+	exchangeItemToLeft(clusters[iCluster], clusters[iClusterNext]);
+}
+
+double calculateClusterErrorsRight(
+		const std::vector<Vector_2> &directions,
+		const std::vector<double> &values,
+		const std::set<int> &cluster,
+		const std::set<int> &clusterNext)
+{
+	std::set<int> clusterChanged = cluster;
+	std::set<int> clusterNextChanged = clusterNext;
+	exchangeItemToRight(clusterChanged, clusterNextChanged);
+	return calculateClusterError(directions, values, clusterChanged) +
+		calculateClusterError(directions, values, clusterNextChanged);
+}
+
+double calculateClusterErrorsLeft(
+		const std::vector<Vector_2> &directions,
+		const std::vector<double> &values,
+		const std::set<int> &cluster,
+		const std::set<int> &clusterNext)
+{
+	std::set<int> clusterChanged = cluster;
+	std::set<int> clusterNextChanged = clusterNext;
+	exchangeItemToLeft(clusterChanged, clusterNextChanged);
+	return calculateClusterError(directions, values, clusterChanged) +
+		calculateClusterError(directions, values, clusterNextChanged);
+}
+
+struct EnhancementDescription
+{
+	int iClusterBestChange;
+	double bestChange;
+	bool ifRightDirection;
+
+	EnhancementDescription() :
+		iClusterBestChange(0),
+		bestChange(0.),
+		ifRightDirection(false) {}
+
+};
+
+EnhancementDescription findBestEnhancement(
+		const std::vector<std::set<int>> &clusters,
+		const std::vector<Vector_2> &directions,
+		const std::vector<double> &values,
+		const std::vector<double> &errors)
+{
+	EnhancementDescription ED;
+	int iCluster = 0;
+	int numClusters = clusters.size();
+	double bestChange = 0.;
+	for (const auto &cluster : clusters)
+	{
+		int iClusterNext = (numClusters + iCluster + 1)
+			% numClusters;
+		const auto &clusterNext = clusters[iClusterNext];
+		double errorInitial = errors[iCluster]
+			+ errors[iClusterNext];
+		double change = errorInitial -
+			calculateClusterErrorsRight(directions,
+				values, cluster, clusterNext);
+		if (change > ED.bestChange)
+		{
+			ED.ifRightDirection = true;
+			ED.iClusterBestChange = iCluster;
+			ED.bestChange = change;
+		}
+
+		change = errorInitial - calculateClusterErrorsLeft(
+				directions, values, cluster,
+				clusterNext);
+		if (change > bestChange)
+		{
+			ED.ifRightDirection = false;
+			ED.iClusterBestChange = iCluster;
+			ED.bestChange = change;
+		}
+
+		++iCluster;
+	}
+	return ED;
+}
+
+void constructBestClusters(
+		std::vector<std::set<int>> &clusters,
+		const std::vector<Vector_2> &directions,
+		const std::vector<double> &values)
+{
+	int numClusters = clusters.size();
+	while (true)
+	{
+		std::vector<double> errors(numClusters);
+		int iCluster = 0;
+		for (const auto &cluster : clusters)
+		{
+			double error = calculateClusterError(directions,
+					values, cluster);
+			errors[iCluster] = error;
+			++iCluster;
+		}
+
+		EnhancementDescription ED = findBestEnhancement(
+				clusters, directions, values, errors);
+
+		if (ED.bestChange > 0.)
+		{
+			if (ED.ifRightDirection)
+				exchangeItemToRight(clusters,
+						ED.iClusterBestChange);
+			else
+				exchangeItemToLeft(clusters,
+						ED.iClusterBestChange);
+		}
+		else
+			break;
+	}
+}
+
 int main(int argc, char **argv)
 {
-	if (argc != 2)
+	if (argc != 3)
 		return EXIT_FAILURE;
 	int numPoints = atoi(argv[1]);
+	int numClusters = atoi(argv[2]);
+
 	std::cout << "Convex hull of " << numPoints << " will be generated"
 		<< std::endl;
 	if (numPoints <= 0)
@@ -173,5 +410,9 @@ int main(int argc, char **argv)
 	std::vector<Vector_2> directions;
 	std::vector<double> values;
 	std::tie(directions, values) = prepareSupportData(points);
+	std::cout << "Data has been prepared." << std::endl;
+
+	auto clusters = prepareInitialClusters(numPoints, numClusters);
+	constructBestClusters(clusters, directions, values);
 	return EXIT_SUCCESS;
 }
