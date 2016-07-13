df39d6c400fb10a4b57e2c657d94a5a91d710b74
Ilya Palachev <iliyapalachev@gmail.com>
2016-07-13 20:58:37 +0300

    [IMPL] recoverer: added option for reading support function data
    Signed-off-by: Ilya Palachev <iliyapalachev@gmail.com>

--- Tests/Recoverer_test.cpp
+++ Tests/Recoverer_test.cpp
@@ -117,6 +117,12 @@
 #define OPTION_SCALE_MATRIX 's'
 
 /**
+ * Option "-S" is used to specify the file name with ready support function
+ * data.
+ */
+#define OPTION_SUPPORT_FUNCTION_DATA_FILE 'S'
+
+/**
  * Option "-t" sets the type of support matrix.
  */
 #define OPTION_SUPPORT_MATRIX_TYPE 't'
@@ -143,7 +149,7 @@
  * Definition of the option set for recoverer test.
  */
 #define RECOVERER_OPTIONS_GETOPT_DESCRIPTION \
-	"a:Abc:d:e:f:F:i:l:m:M:n:N:o:p:r:st:T:vx"
+	"a:Abc:d:e:f:F:i:l:m:M:n:N:o:p:r:sS:t:T:vx"
 
 struct PCLOption: public option
 {
@@ -385,6 +391,16 @@ static PCLOption optionsLong[] =
 		},
 		"The value of threshold of angle between adjacent facets."
 	},
+	{
+		option
+		{
+			"support-function-data",
+			required_argument,
+			0,
+			OPTION_SUPPORT_FUNCTION_DATA_FILE
+		},
+		"Support function data file."
+	},
 	{option{0, 0, 0, 0}, 0}
 };
 
@@ -452,8 +468,17 @@ typedef struct
 	/** The string with intput data location. */
 	union
 	{
-		/** File name with existent shadow contour data. */
-		char *fileName;
+		struct
+		{
+			/**
+			 * File name with existent shadow contour data or
+			 * support function data.
+			 */
+			char *name;
+
+			/** Whether it is support function data or not. */
+			bool ifSupportFunctionData;
+		} file;
 
 		/** Parameters of synthetically generated data. */
 		SyntheticModelDescription model;
@@ -901,6 +926,7 @@ static CommandLineOptions* parseCommandLine(int argc, char** argv)
 	options->numFinitePlanes = 0;
 	options->fileNamePolyhedron = NULL;
 	options->threshold = 0.;
+	options->input.file.ifSupportFunctionData = false;
 	int optionIndex = 0;
 	while ((charCurr = getopt_long(argc, argv,
 		RECOVERER_OPTIONS_GETOPT_DESCRIPTION, optionsLong,
@@ -916,6 +942,8 @@ static CommandLineOptions* parseCommandLine(int argc, char** argv)
 			DEBUG_END;
 			exit(EXIT_FAILURE);
 			break;
+		case OPTION_SUPPORT_FUNCTION_DATA_FILE:
+			options->input.file.ifSupportFunctionData = true;
 		case OPTION_FILE_NAME:
 			if (ifOptionFileName)
 			{
@@ -923,7 +951,7 @@ static CommandLineOptions* parseCommandLine(int argc, char** argv)
 			}
 
 			options->mode = RECOVERER_REAL_TESTING;
-			options->input.fileName = optarg;
+			options->input.file.name = optarg;
 			ifOptionFileName = true;
 			break;
 		case OPTION_MODEL_NAME:
@@ -1428,7 +1456,7 @@ static CommandLineOptions* parseCommandLine(int argc, char** argv)
 	{
 		if (ifOptionFileName)
 		{
-			options->outputName = strdup(options->input.fileName);
+			options->outputName = strdup(options->input.file.name);
 		}
 		else
 		{
@@ -1562,7 +1590,8 @@ static ShadowContourDataPtr getRealSCData(CommandLineOptions* options)
 	ShadowContourDataPtr SCData(new ShadowContourData(p));
 
 	/* Read shadow contours data from file. */
-	SCData->fscanDefault(options->input.fileName);
+	ASSERT(!options->input.file.ifSupportFunctionData);
+	SCData->fscanDefault(options->input.file.name);
 
 	DEBUG_END;
 	return SCData;
@@ -1733,9 +1762,31 @@ static std::vector<Point_3> readDirections(char *fileName)
 	return points;
 }
 
-static SupportFunctionDataPtr makeSupportData(CommandLineOptions* options)
+static SupportFunctionDataPtr makeSupportData(CommandLineOptions *options)
 {
 	DEBUG_START;
+	if (options->mode == RECOVERER_REAL_TESTING
+			&& options->input.file.ifSupportFunctionData)
+	{
+		std::filebuf fb;
+		if (fb.open(options->input.file.name, std::ios::in))
+		{
+			std::istream is(&fb);
+			SupportFunctionDataPtr data(new SupportFunctionData(
+				is));
+			fb.close();
+			DEBUG_END;
+			return data;
+		}
+		else
+		{
+			ERROR_PRINT("Cannot open file %s",
+				options->input.file.name);
+			DEBUG_END;
+			exit(EXIT_FAILURE);
+		}
+	}
+
 	/* Create polyhedron based on one of possible models. */
 	Polyhedron_3 p = makeModel(options);
 	globalPCLDumper(PCL_DUMPER_LEVEL_DEBUG,
@@ -1905,9 +1956,10 @@ int main(int argc, char** argv)
 	/* Create the recoverer with requested properties. */
 	RecovererPtr recoverer = makeRecoverer(options);
 	
-	if (options->mode == RECOVERER_REAL_TESTING
-			|| (options->mode == RECOVERER_SYNTHETIC_TESTING
-				&& options->input.model.numContours))
+	if ((options->mode == RECOVERER_REAL_TESTING
+		&& !options->input.file.ifSupportFunctionData)
+		|| (options->mode == RECOVERER_SYNTHETIC_TESTING
+		&& options->input.model.numContours))
 	{
 		/*
 		 * Read or generate shadow contour data depending on requested
