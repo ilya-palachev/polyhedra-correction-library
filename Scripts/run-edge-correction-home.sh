cba41b615508e59e342bbdc5327a22271270cc8d
Ilya Palachev <iliyapalachev@gmail.com>
2016-12-18 20:33:28 +0300

    [DEBUG] scripts: update the script
    Signed-off-by: Ilya Palachev <iliyapalachev@gmail.com>

--- Scripts/run-edge-correction-home.sh
+++ Scripts/run-edge-correction-home.sh
@@ -4,4 +4,4 @@ WORKSPACE_DIR="/home/ilya/git/polyhedra-correction-library"
 SOURCE_DIR=${WORKSPACE_DIR}
 BUILD_DIR=${WORKSPACE_DIR}/buildRelease
 
-EDGE_CORRECTION=1 ${WORKSPACE_DIR}/buildRelease/Recoverer_test -f poly-data-in/1_GIA/shadow_contours.dat -o poly-data-out/1_GIA -r native -p l2 -x -s -b -T 0.01 -z 0.01
+Z_LOWER=0. Z_UPPER=1.5 EDGE_CORRECTION=1 ${WORKSPACE_DIR}/buildRelease/Recoverer_test -f poly-data-in/1_GIA/shadow_contours.dat -o poly-data-out/1_GIA -r native -p l2 -x -s -b -T 0.01 -z 0.01
