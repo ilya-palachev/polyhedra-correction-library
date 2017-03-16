0e43f1c15e64e79b80949ff03adf26b700d1f4af
Ilya Palachev <iliyapalachev@gmail.com>
2017-03-16 10:34:22 +0300

    [REFACTOR] nq-estimator: moved code and reorganized structures
    Signed-off-by: Ilya Palachev <iliyapalachev@gmail.com>

--- Source/Recoverer/NativeQuadraticEstimator.cpp
+++ Source/Recoverer/NativeQuadraticEstimator.cpp
@@ -36,17 +36,24 @@ typedef CGAL::Delaunay_triangulation_3<Kernel, TDataStructure> TDelaunay_3;
 typedef TDelaunay_3::Vertex_handle Vertex_handle;
 typedef TDelaunay_3::Cell_handle Cell_handle;
 
+struct SupportItem
+{
+	Vector_3 direction;
+	Plane_3 plane;
+	std::set<Cell_handle> associations;
+};
+
 class DualPolyhedron_3 : public TDelaunay_3
 {
 private:
-	std::vector<Vector_3> directions;
-	std::vector<Plane_3> planes;
+	std::vector<SupportItem> items;
 	std::set<Vertex_handle> outerVertices;
 	std::set<Vertex_handle> innerVertices;
 
 	bool isOuterVertex(const Vertex_handle &vertex) const;
 	void associateVertex(const Vertex_handle &vertex);
 	void initializeVertices();
+	void verify() const;
 public:
 	typedef std::pair<Point_3, unsigned> PointIndexed_3;
 
@@ -54,10 +61,19 @@ public:
 			const std::vector<Plane_3> &planes,
 			const std::vector<PointIndexed_3>::iterator &begin,
 			const std::vector<PointIndexed_3>::iterator &end):
-		TDelaunay_3(begin, end),
-		directions(directions),
-		planes(planes)
-	{}
+		TDelaunay_3(begin, end)
+	{
+		DEBUG_START;
+		ASSERT(directions.size() == planes.size() && "Wrong input");
+		for (unsigned i = 0; i < planes.size(); ++i)
+		{
+			SupportItem item;
+			item.direction = directions[i];
+			item.plane = planes[i];
+			items.push_back(item);
+		}
+		DEBUG_END;
+	}
 
 	void initialize();
 };
@@ -100,66 +116,6 @@ bool DualPolyhedron_3::isOuterVertex(const Vertex_handle &vertex) const
 			unusedIndex0, unusedIndex1);
 	DEBUG_END;
 	return result;
-}
-
-void DualPolyhedron_3::initializeVertices()
-{
-	DEBUG_START;
-	for (auto I = finite_vertices_begin(), E = finite_vertices_end();
-			I != E; ++I)
-	{
-		Vertex_handle vertex = I;
-		if (isOuterVertex(vertex))
-			outerVertices.insert(vertex);
-		else
-			innerVertices.insert(vertex);
-	}
-	std::cout << "Number of outer vertices: " << outerVertices.size()
-		<< std::endl;
-	std::cout << "Number of inner vertices: " << innerVertices.size()
-		<< std::endl;
-	ASSERT(outerVertices.size() + innerVertices.size() == planes.size()
-		&& "Different numbers of dual vertices and primal planes");
-	DEBUG_END;
-}
-
-void DualPolyhedron_3::initialize()
-{
-	DEBUG_START;
-	initializeVertices();
-
-	std::vector<Vertex_handle> vertices;
-	for (auto I = finite_vertices_begin(), E = finite_vertices_end();
-			I != E; ++I)
-		associateVertex(I);
-
-	if (getenv("INTERNAL_CHECK"))
-	{
-		std::vector<Cell_handle> outerCells;
-		incident_cells(infinite_vertex(),
-				std::back_inserter(outerCells));
-		std::cout << "Number of outer cells: " << outerCells.size()
-			<< std::endl;
-
-		Polyhedron_3 intersection(planes);
-		ASSERT(intersection.size_of_vertices() == outerCells.size()
-			&& "Two methods must produce the same topology");
-
-		unsigned numEmptyCells = 0;
-		for (Cell_handle cell : outerCells)
-		{
-			std::cout << "Cell indices: ";
-			for (int iPlane : cell->info())
-				std::cout << iPlane << " ";
-			std::cout << std::endl;
-			numEmptyCells += cell->info().empty();
-		}
-		std::cout << "Number of empty outer cells: " << numEmptyCells
-			<< std::endl;
-		ASSERT(numEmptyCells == 0
-				&& "All outer cells should be nonempty");
-	}
-	DEBUG_END;
 }
 
 static double calculateProduct(const Vector_3 &direction,
@@ -205,8 +161,6 @@ static Cell_handle findBestCell(const Vector_3 &direction,
 		}
 	} while (nextCell != bestCell);
 	ASSERT(bestCell->has_vertex(infinity) && "Wrong result");
-	std::cout << "Found value " << maxProduct << " in " << numIterations
-		<< " iterations" << std::endl;
 	DEBUG_END;
 	return bestCell;
 }
@@ -234,16 +188,80 @@ void DualPolyhedron_3::associateVertex(const Vertex_handle &vertex)
 			currentCell->info().insert(iPlane);
 			++circulator;
 		} while (circulator != end);
-		
 	}
 	else
 	{
 		ASSERT(!isOuterVertex(vertex) && "Wrong set");
-		Vector_3 direction = directions[iPlane];
+		Vector_3 direction = items[iPlane].direction;
 		Cell_handle bestCell = findBestCell(direction,
 				infinite_vertex(), infinite_cell());
 		bestCell->info().insert(iPlane);
 	}
+	DEBUG_END;
+}
+
+void DualPolyhedron_3::initializeVertices()
+{
+	DEBUG_START;
+	for (auto I = finite_vertices_begin(), E = finite_vertices_end();
+			I != E; ++I)
+	{
+		Vertex_handle vertex = I;
+		if (isOuterVertex(vertex))
+			outerVertices.insert(vertex);
+		else
+			innerVertices.insert(vertex);
+	}
+	std::cout << "Number of outer vertices: " << outerVertices.size()
+		<< std::endl;
+	std::cout << "Number of inner vertices: " << innerVertices.size()
+		<< std::endl;
+	ASSERT(outerVertices.size() + innerVertices.size() == items.size()
+		&& "Different numbers of dual vertices and primal planes");
+	DEBUG_END;
+}
+
+void DualPolyhedron_3::verify() const
+{
+	DEBUG_START;
+	std::vector<Cell_handle> outerCells;
+	incident_cells(infinite_vertex(), std::back_inserter(outerCells));
+	std::cout << "Number of outer cells: " << outerCells.size()
+		<< std::endl;
+
+	std::vector<Plane_3> planes;
+	for (const SupportItem &item : items)
+		planes.push_back(item.plane);
+	Polyhedron_3 intersection(planes);
+	ASSERT(intersection.size_of_vertices() == outerCells.size()
+		&& "Two methods must produce the same topology");
+
+	unsigned numEmptyCells = 0;
+	for (Cell_handle cell : outerCells)
+	{
+		std::cout << "Cell indices: ";
+		for (int iPlane : cell->info())
+			std::cout << iPlane << " ";
+		std::cout << std::endl;
+		numEmptyCells += cell->info().empty();
+	}
+	std::cout << "Number of empty outer cells: " << numEmptyCells
+		<< std::endl;
+	ASSERT(numEmptyCells == 0 && "All outer cells should be nonempty");
+	DEBUG_END;
+}
+
+void DualPolyhedron_3::initialize()
+{
+	DEBUG_START;
+	initializeVertices();
+
+	for (auto I = finite_vertices_begin(), E = finite_vertices_end();
+			I != E; ++I)
+		associateVertex(I);
+
+	if (getenv("INTERNAL_CHECK"))
+		verify();
 	DEBUG_END;
 }
 
