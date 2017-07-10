6ca1de878a59d39ea1be9b5ef203c29177e2c3b1
Ilya Palachev <iliyapalachev@gmail.com>
2017-07-10 18:28:24 +0300

    [IMPL] corrector: start implementation
    Signed-off-by: Ilya Palachev <iliyapalachev@gmail.com>

---
+++ Tests/correct.cpp
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2017 Ilya Palachev <iliyapalachev@gmail.com>
+ *
+ * This file is part of Polyhedra Correction Library.
+ *
+ * Polyhedra Correction Library is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Polyhedra Correction Library is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Polyhedra Correction Library.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file correct.cpp
+ * @brief Corrections of polyhedrons by the information about their shadow and
+ * blink contours.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "DebugPrint.h"
+#include "DebugAssert.h"
+#include "Polyhedron/Polyhedron.h"
+
+static void printUsage(const char *name)
+{
+	DEBUG_START;
+	printf("Usage: %s <polyhedron.dat> <edges.dat>\n", name);
+	DEBUG_END;
+}
+
+int main(int argc, char **argv)
+{
+	DEBUG_START;
+	if (argc < 3)
+	{
+		printUsage(argv[0]);
+		DEBUG_END;
+		return EXIT_FAILURE;
+	}
+
+	const char *pathPolyhedron = argv[1];
+	const char *pathEdgesData = argv[2];
+	Polyhedron p;
+	if (!p.fscan_default_1_2(pathPolyhedron))
+	{
+		printf("Failed to read polyhedron data!\n");
+		printUsage(argv[0]);
+		DEBUG_END;
+		return EXIT_FAILURE;
+	}
+
+	ASSERT(pathEdgesData);
+
+	DEBUG_END;
+	return EXIT_SUCCESS;
+}
