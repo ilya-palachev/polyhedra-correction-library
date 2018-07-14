1809cc5494f3427e07c3cb09135f4fe7dfe43adc
Ilya Palachev <palachev.ilya@yandex.ru>
2018-07-14 21:17:38 +0300

    [IMPL] ht-detector: added symmetric lens example from paper
    Signed-off-by: Ilya Palachev <palachev.ilya@yandex.ru>

---
+++ Tests/detectSymmetricLensCorners.cpp
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2009-2018 Ilya Palachev <iliyapalachev@gmail.com>
+ *
+ * This file is part of Polyhedra Correction Library.
+ *
+ * Polyhedra Correction Library is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Polyhedra Correction Library is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Polyhedra Correction Library.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file detectSymmetricLensCorners.cpp
+ * @brief Detection of corners in the horizontal lens, i.e. intersection of two
+ * 2D disks:
+ *   (x - 2)^2 + y^2 <= 9
+ * and
+ *   (x + 2)^2 + y^2 <= 9.
+ * This is an example for original Halls and Turlach paper.
+ */
+
+#include <cstdlib>
+#include "PolyhedraCorrectionLibrary.h"
+#include "Recoverer/HallTurlachDetector.h"
+
+
+static ItemsVector generateSymmetricLensItems(unsigned N)
+{
+  /* FIXME: Implement normal noise. */
+  ItemsVector items;
+  for (unsigned i = 0; i < N; ++i) {
+    double theta = (2. * M_PI * (double(i) + 0.5)) / N - M_PI;
+    double h = 0.;
+    double arctg = atan2(sqrt(5), 2);
+    double theta1entry = M_PI * 0.5 - arctg;
+    double theta1exit = M_PI - theta1entry;
+    double theta2entry = -theta1exit;
+    double theta2exit = -theta1entry;
+    if (theta1entry <= theta && theta <= theta1exit)
+      h = sqrt(5) * sin(theta);
+    else if (theta2entry <= theta && theta <= theta2exit)
+      h = -sqrt(5) * sin(theta);
+    else if (theta2exit <= theta && theta <= theta1entry)
+      h = 3. - 2. * cos(theta);
+    else
+      h = 3. + 2. * cos(theta);
+    items.push_back(std::make_pair(theta, h));
+  }
+  return items;
+}
+
+int main(int argc, char **argv) {
+  DEBUG_START;
+  /* Parse command line. */
+  if (argc != 6) {
+    fprintf(stderr, "Usage: %s N m t l q\n", argv[0]);
+    return EXIT_FAILURE;
+  }
+  /* Read the "N" value, i.e. number of thetas. */
+  unsigned NParameter = atoi(argv[1]);
+
+  /* Read the "m" value, i.e. p'' + p averaging parameter. */
+  unsigned mParameter = atoi(argv[2]);
+
+  /* Read the "t" value, i.e. the upper bound for small numbers. */
+  char *mistake = NULL;
+  double tParameter = strtod(argv[3], &mistake);
+  if (mistake && *mistake) {
+    fprintf(stderr, "Error while reading t = %s\n", argv[4]);
+    exit(EXIT_FAILURE);
+  }
+
+  /* Read the "l" value, i.e. step for p' estimate calculation. */
+  unsigned lParameter = atoi(argv[4]);
+
+  /* Read the "q" value, i.e. the final estimate averaging parameter. */
+  mistake = NULL;
+  double qParameter = strtod(argv[5], &mistake);
+  if (mistake && *mistake) {
+    fprintf(stderr, "Error while reading q = %s\n", argv[6]);
+    exit(EXIT_FAILURE);
+  }
+  auto items = generateSymmetricLensItems(NParameter);
+  auto corners = estimateCorners(items, mParameter, tParameter, lParameter,
+                                 qParameter, true);
+  fprintf(stdout, "%lu corners have been detected.\n", corners.size());
+  DEBUG_END;
+  return EXIT_SUCCESS;
+}
