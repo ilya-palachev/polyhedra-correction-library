199f27eca90c7b3e4bda5f59d92705dbe7661e0f
Ilya Palachev <palachev.ilya@yandex.ru>
2018-07-14 13:11:01 +0300

    [IMPL] ht-detector: Added script for good corner detection
    Signed-off-by: Ilya Palachev <palachev.ilya@yandex.ru>

---
+++ Scripts/corner-detection-good.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH="/home/ilya/thesis/Ipopt/install/release/lib"
+PCL_PATH="/home/ilya/thesis/polyhedra-correction-library/"
+
+"${PCL_PATH}""/buildRelease/calculateHorizontalSection" "poly-data-in/1_GIA/polyhedron.dat" -5 | tee model.txt
+cat model.txt | grep "^    x = " | sed -e "s/    x = //g;s/, y =//g" > model-plot.txt
+
+"${PCL_PATH}""/buildRelease/HallTurlachDetector" "poly-data-in/1_GIA/shadow_contours.dat" -5 10 20. 4 0.5 | tee estimate.txt
+cat estimate.txt | grep "^    x = " | sed -e "s/    x = //g;s/, y =//g" > estimate-plot.txt
+
+gnuplot -persist -e "plot 'model-plot.txt' using 1:2 with lines, 'estimate-plot.txt' using 1:2 with lines;"
